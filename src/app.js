a9a74a9dda055c1775e43a1e947451e23b5ea3b3
ashok.atce <ashok.atce@gmail.com>
2024-08-26 06:55:40 +0530

    Revert "modified env file"
    This reverts commit 73a1d20cd27ce2b0b9024c375151434a87999085.

--- src/app.js
+++ src/app.js
@@ -1,17 +1,14 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from 'cors'
-import dotenv from "dotenv"
 const app=express();
-
-dotenv.config();
 
 // Set up middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("../public"));
 app.use(cookieParser())
-app.use(cors({origin:[process.env.DEV_ORIGIN, process.env.PROD_ORIGIN],credentials:true}));
+app.use(cors({origin:["http://localhost:5173","http://34.93.216.160"],credentials:true}));
 
 //import routes
 import userRoutes from '../src/routes/user.routes.js'
