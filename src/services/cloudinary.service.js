bbb543772519ad050b35ec494a65b7589a60edd7
ashok.atce <ashok.atce@gmail.com>
2024-08-29 12:12:47 +0530

    modify cloudinary file

--- src/services/cloudinary.service.js
+++ src/services/cloudinary.service.js
@@ -15,7 +15,8 @@ const uploadOnCloudinary=async (localFilePath)=>{
         
         //upload file on cloudinary
         const response=await cloudinary.uploader.upload(localFilePath,{
-            resource_type:'auto'
+            resource_type:'auto',
+            folder:'myblog/upload'
         })
 
         //delete file from local path
