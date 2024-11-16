c551c544f3193ed85f7c8e3a1cdcff0b0591d376
ashok.atce <ashok.atce@gmail.com>
2024-11-16 17:29:39 +0530

    optimize error handling of upload blog image

--- src/routes/blog.routes.js
+++ src/routes/blog.routes.js
@@ -7,14 +7,14 @@ const router=Router();
 
 
 //Prefix: api/blogs/
-router.post('/create-blog', authMiddleware, upload.single('feature_image'), fileValidation, createBlog)
+router.post('/create-blog', authMiddleware, upload.single('feature_image'), fileValidation, createBlog);
 router.get('/get-blogs',getBlogs);
 router.get('/latest-blogs',getLatestBlogs);
 router.get('/get-blog/:slug',getBlogBySlug);
-router.patch('/update-blog/:blogId',authMiddleware, upload.single('feature_image'), fileValidation, updateBlog)
-router.delete('/delete-blog/:blogId',authMiddleware, deleteBlog)
-router.patch('/:blogId/like',authMiddleware,likeBlog)
-router.patch('/:blogId/comment',authMiddleware,addComment)
-router.get('/test',(req,res)=>res.send("test message"))
+router.patch('/update-blog/:blogId', authMiddleware, upload.single('feature_image'), fileValidation, updateBlog);
+router.delete('/delete-blog/:blogId',authMiddleware, deleteBlog);
+router.patch('/:blogId/like',authMiddleware,likeBlog);
+router.patch('/:blogId/comment',authMiddleware,addComment);
+router.get('/test',(req,res)=>res.send("test message"));
 
 export default router;
